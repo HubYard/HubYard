53d5fe9a8acb8fca05b14d93c5c578d0f96891d9
Shubham Naik <shubham.naik10@gmail.com>
2015-02-15 18:13:39 -0800

    fix

---
+++ keys.js
@@ -0,0 +1,23 @@
+module.exports = {	
+        twitterconsume:"",
+        twitterconsume_secret:"",
+        instagramconsume:'',
+        instagramconsume_secret:'',
+        producthuntconsume:'',
+        producthuntconsume_secret:'',
+        dribbleconsume:'',
+        dribbleconsume_secret:'',
+        tumblrconsume:"",
+        tumblrconsume_secret:"",
+        facebookconsume:"",
+        facebookconsume_secret:"",
+        url_host:'',
+        stripe:"",
+        connection_string:'',
+        email:{	
+            host		: '',
+            user 		: '',
+            password 	: '',
+            sender		: ''
+        }
+}
