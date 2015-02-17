a3dbafe749160c27a6b48dc638b6c24a102cb3ea
Araan Branco <araan10@hotmail.com>
2015-02-16 23:45:46 -0200

    Added separate environments

---
+++ env/production.js
@@ -0,0 +1,25 @@
+'use restrict';
+
+module.exports = {	
+        twitterconsume:"",
+        twitterconsume_secret:"",
+        instagramconsume:'',
+        instagramconsume_secret:'',
+        producthuntconsume:'',
+        producthuntconsume_secret:'',
+        dribbleconsume:'',
+        dribbleconsume_secret:'',
+        tumblrconsume:"",
+        tumblrconsume_secret:"",
+        facebookconsume:"",
+        facebookconsume_secret:"",
+        url_host:'',
+        stripe:"",
+        connection_string:'',
+        email:{	
+            host		: '',
+            user 		: '',
+            password 	: '',
+            sender		: ''
+        }
+}
